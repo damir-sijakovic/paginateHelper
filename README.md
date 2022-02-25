338217b9ce06d4c5e9d58b8980e87762105a58c3
ddd <ddd@debian-BULLSEYE-live-builder-AMD64>
2022-02-25 19:31:32 +0100

    1645813892

---
+++ README.md
@@ -0,0 +1,45 @@
+               ,-,--.   .=-.-.     ,--.-,  ,---.      ,--.-.,-.  
+  _,..---._  ,-.'-  _\ /==/_ /    |==' -|.--.'  \    /==/- |\  \ 
+/==/,   -  \/==/_ ,_.'|==|, |     |==|- |\==\-/\ \   |==|_ `/_ / 
+|==|   _   _\==\  \   |==|  |   __|==|, |/==/-|_\ |  |==| ,   /  
+|==|  .=.   |\==\ -\  |==|- |,--.-'\=|- |\==\,   - \ |==|-  .|   
+|==|,|   | -|_\==\ ,\ |==| ,||==|- |=/ ,|/==/ -   ,| |==| _ , \  
+|==|  '='   /==/\/ _ ||==|- ||==|. /=| -/==/-  /\ - \/==/  '\  | 
+|==|-,   _`/\==\ - , //==/. /\==\, `-' /\==\ _.\=\.-'\==\ /\=\.' 
+`-.`.____.'  `--`---' `--`-`  `--`----'  `--`         `--`       
+ 
+>>>> MAGIC PAGINATE HELPER FUNCTION !!1 >>>>
+                                                                                                                                                      
+WHAT IS IT?
+
+Function that helps you handle pagination.
+
+HOW TO USE?
+
+paginateHelper($itemNumber, $perPage, $currentPage, $visiblePages=8)
+
+$itemNumber - number of all items in db
+$perPage - items on page
+$currentPage - on what page we are currently
+$visiblePages - number of visible pages in middle 
+
+AND THIS RETURNS?
+
+An array with this data:
+
+$arr['itemNumber'] =             $itemNumber;
+$arr['indexlastPageItemEnd'] =   $lastPageItemEnd;
+$arr['indexlastPageItemStart'] = $lastPageItemStart;
+$arr['perPage'] =                $perPage;
+$arr['currentPage'] =            $currentPage;
+$arr['indexOffset'] =            $limitFirst;     //index, SQL LIMIT starts from 0
+$arr['indexRowCount'] =          $perPage;        //index+length
+$arr['numberOfPages'] =          $numberOfPages;
+$arr['visibleStartPage'] =       $groupStart;
+$arr['visibleEndPage'] =         $groupEnd;									 
+$arr['firstPage'] = 1;           
+$arr['lastPage'] =               $numberOfPages;	
+$arr['prevPage'] =               $prevPage;	
+$arr['nextPage'] =               $nextPage;	
+
+HAVE FUN!
---
+++ paginateHelper.php
@@ -0,0 +1,102 @@
+<?php
+
+//NOTE THAT SQL LIMIT STARTS AS ARRAY, FROM 0
+function paginateHelper($itemNumber, $perPage, $currentPage, $visiblePages=8)
+{	
+	if (!is_int($itemNumber) || $itemNumber === 0)
+	{
+		return null;
+	}
+	
+	$perPage = intval($perPage);
+	$currentPage = intval($currentPage);
+			
+	if ($perPage > $itemNumber)
+	{
+		$perPage = $itemNumber;
+	}
+	
+	if ($perPage < 1)
+	{
+		$perPage = $itemNumber;
+	}
+		
+	if ($currentPage < 1)
+	{
+		$currentPage = 1;
+	}
+			
+	$numberOfPages = intval(ceil($itemNumber/$perPage));	
+	$limitCurrentPage = $currentPage - 1;
+	$limitFirst = $perPage * $limitCurrentPage;
+	$lastPageItemEnd = $perPage * $numberOfPages;
+	$lastPageItemStart = $lastPageItemEnd - $perPage;			
+	$visiblePageRange = [];		
+	
+	if ($currentPage > $numberOfPages)
+	{		
+		$currentPage = $numberOfPages;	
+		$limitFirst = $lastPageItemStart;
+	}		
+	
+	$groupStart = 1;
+	$groupEnd = $visiblePages;
+	for ($i=1; $i<$numberOfPages; $i=$i+$visiblePages)
+	{			
+		if ($currentPage<$i)
+		{ 
+			if ($i+$visiblePages > $numberOfPages)
+			{
+				$groupEnd = $i-1;
+			}
+	
+			break;
+		}
+		$groupStart = $i;
+		if (($i+$visiblePages) > $numberOfPages){
+			
+			$groupEnd = $numberOfPages;		
+		}
+	}
+	
+	
+	if ($currentPage > 1)
+	{
+		$prevPage = $currentPage - 1;
+	}
+	else
+	{
+		$prevPage = null;
+	}
+	
+	
+	if ($currentPage < $numberOfPages)
+	{
+		$nextPage = $currentPage + 1;
+	}
+	else
+	{
+		$nextPage = null;
+	}
+					
+	
+	$arr = [];
+	$arr['itemNumber'] =             $itemNumber;
+	$arr['indexlastPageItemEnd'] =   $lastPageItemEnd;
+	$arr['indexlastPageItemStart'] = $lastPageItemStart;
+	$arr['perPage'] =                $perPage;
+	$arr['currentPage'] =            $currentPage;
+	$arr['indexOffset'] =            $limitFirst; //index, SQL LIMIT starts from 0
+	$arr['indexRowCount'] =          $perPage; //index+length
+	$arr['numberOfPages'] =          $numberOfPages;
+	$arr['visibleStartPage'] =       $groupStart;
+	$arr['visibleEndPage'] =         $groupEnd;	
+									 
+	$arr['firstPage'] = 1;           
+	$arr['lastPage'] = $numberOfPages;	
+	$arr['prevPage'] = $prevPage;	
+	$arr['nextPage'] = $nextPage;	
+			
+	return $arr;
+	
+} 
